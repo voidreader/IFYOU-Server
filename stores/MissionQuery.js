807012a68a7c555ffcc3676b96f8af42416e612e
ije90 <ije90@pi-er.com>
2022-01-06 13:54:09 +0900

    아이콘 이미지 추가

--- stores/MissionQuery.js
+++ stores/MissionQuery.js
@@ -45,6 +45,8 @@ WHERE mission_id  = ?;
 export const MQ_CLIENT_SELECT_MISSION = `
 SELECT a.mission_id, mission_name, mission_hint, is_hidden, reward_currency, reward_quantity, reward_exp, 
 image_url, image_key, unlock_state
+, fn_get_design_info((SELECT icon_image_id FROM com_currency WHERE currency = reward_currency), 'url') icon_image_url
+, fn_get_design_info((SELECT icon_image_id FROM com_currency WHERE currency = reward_currency), 'key') icon_image_key
 FROM list_mission a LEFT OUTER JOIN user_mission b ON a.mission_id = b.mission_id AND userkey = ?
 WHERE project_id = ?;
 `;
