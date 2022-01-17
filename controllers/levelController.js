dbed909446432f1da76a380969f7693cd0b156b1
ije90 <ije90@pi-er.com>
2022-01-17 19:22:55 +0900

    레벨업 수정 완료

--- controllers/levelController.js
+++ controllers/levelController.js
@@ -32,10 +32,22 @@ export const updateUserLevelProcess = async (req, res) => {
   let updateQuery = ``;
   let sendQuery = ``;
 
+  let total_experience = current_experience+experience;
+  let level_bonus_check = 0; 
+  let target_level = 0;
+  let target_experience = 0;
+  let target_currency = ``;
+  let target_quantity = 0;
+  let icon_image_url = ``; 
+  let icon_image_key = ``; 
+  let currency_type = ``;
+  let type_name = ``;
+  let currency_name = ``;
+
   //* 다음 레벨 목표 경험치, 재화, 개수 가져오기
   let result = await DB(
-    `
-    SELECT experience 
+    `SELECT next_level 
+    , experience 
     , a.currency
     , quantity 
     , fn_get_design_info(icon_image_id, 'url') icon_image_url
@@ -45,61 +57,82 @@ export const updateUserLevelProcess = async (req, res) => {
     , fn_get_localize_text(b.local_code, '${lang}') currency_name
     FROM com_level_management a, com_currency b, list_standard ls 
     WHERE a.currency = b.currency 
-     AND ls.standard_class = 'currency_type'
-     AND ls.code = b.currency_type
-     AND next_level = ?;
-    `,
-    [current_level + 1]
+    AND ls.standard_class = 'currency_type'
+    AND ls.code = b.currency_type
+    AND next_level > ${current_level}
+    AND experience <= ${total_experience};`
   );
-  const next_experience = result.row[0].experience;
-  const next_currency = result.row[0].currency;
-  const next_quantity = result.row[0].quantity;
-  const {
-    icon_image_url,
-    icon_image_key,
-    currency_type,
-    type_name,
-    currency_name,
-  } = result.row[0];
-
-  //* 경험치 쌓기, 레벨업 처리
-  let target_level = 0;
-  let target_experience = 0;
-  let target_currency = ``;
-  if (next_experience <= current_experience + experience) {
-    //레벨업 하는 경우
-    target_level = current_level + 1;
-    target_experience = current_experience + experience - next_experience;
-    target_currency = next_currency;
-  } else {
-    target_level = current_level;
-    target_experience = current_experience + experience;
-  }
-
-  //* 2022.01.13 JE - 레벨업 이벤트 추가 보상(단발성 : 2022.01.19 ~ 25일 23시 59분)
-  //* 해당 되는 레벨(5, 8, 11, 15)들은 메일 발송 처리 
-  let level_bonus_check = 0; 
-  result = await DB(`
-  SELECT CASE WHEN ${target_level} = 5 OR ${target_level} = 8 OR ${target_level} = 11 OR ${target_level} = 15 THEN 1 ELSE 0 END level_bonus_check
-  FROM DUAL WHERE now() between '2022-01-19 00:00:00' AND '2022-01-25 23:59:59';`);
   if(result.row.length > 0){
-    level_bonus_check = result.row[0].level_bonus_check; 
+    // eslint-disable-next-line no-restricted-syntax
+    for(const item of result.row){
+      total_experience -= item.experience; 
+      if(total_experience > 0) {
+        currentQuery = `CALL sp_insert_user_property(?,?,?,?);`;
+        sendQuery += mysql.format(currentQuery, [
+          userkey,
+          item.currency,
+          item.quantity,
+          "levelup",
+        ]);
+    
+        //* 2022.01.13 JE - 레벨업 이벤트 추가 보상(단발성 : 2022.01.19 ~ 25일 23시 59분)
+        //* 해당 되는 레벨(5, 8, 11, 15)들은 메일 발송 처리 
+        // eslint-disable-next-line no-await-in-loop
+        result = await DB(`
+        SELECT CASE WHEN ${item.next_level} = 5 OR ${item.next_level} = 8 OR ${item.next_level} = 11 OR ${item.next_level} = 15 THEN 1 ELSE 0 END level_bonus_check
+        FROM DUAL WHERE now() between '2022-01-17 00:00:00' AND '2022-01-25 23:59:59';`);
+        if(result.state && result.row.length > 0){
+    
+          level_bonus_check = result.row[0].level_bonus_check; 
+          currentQuery = `INSERT INTO user_mail(userkey, mail_type, currency, quantity, expire_date, connected_project) 
+          VALUES(?, 'event', ?, ?, DATE_ADD(NOW(), INTERVAL 1 YEAR), -1);`;
+          if(level_bonus_check === 1){
+            if(item.next_level === 5){
+              sendQuery += mysql.format(currentQuery, [userkey,"gem","3"]);
+            }else if(item.next_level === 8){
+              sendQuery += mysql.format(currentQuery, [userkey,"gem","3"]);
+              sendQuery += mysql.format(currentQuery, [userkey,"coin","500"]);
+            }else if(item.next_level === 11){
+              sendQuery += mysql.format(currentQuery, [userkey,"gem","5"]);
+              sendQuery += mysql.format(currentQuery, [userkey,"coin","1000"]);    
+            }else if(item.next_level === 15){
+              sendQuery += mysql.format(currentQuery, [userkey,"gem","5"]);
+              sendQuery += mysql.format(currentQuery, [userkey,"coin","1500"]);
+            }
+          }
+        }
+        
+        target_level = item.next_level;
+        target_currency = item.currency; 
+        target_quantity = item.quantity;
+        target_experience = total_experience; 
+        icon_image_url = item.icon_image_keyl;
+        icon_image_key = item.icon_image_key;
+        currency_type = item.currency_type;
+        type_name = item.type_name; 
+        currency_name = item.currency_name;
+      }
+    }
+  }else{
+    target_level = current_level;
+    target_experience = total_experience; 
   }
 
-  //* 레벨 히스토리 누적
+
+  //* 레벨 히스토리 누적 > 최종 currency로 히스토리 누적 
   currentQuery = `
-    INSERT INTO user_level_history(userkey, current_level, experience, route, clear_id, project_id, currency, is_event ) 
-    VALUES(?, ?, ?, ?, ?, ?, ?, ?);`;
+  INSERT INTO user_level_history(userkey, current_level, experience, route, clear_id, project_id, currency, is_event ) 
+  VALUES(?, ?, ?, ?, ?, ?, ?, ?);`;
   insertQuery = mysql.format(currentQuery, [
-    userkey,
-    current_level,
-    experience,
-    route,
-    clear_id,
-    project_id,
-    target_currency,
-    level_bonus_check, 
-  ]);
+  userkey,
+  current_level,
+  experience,
+  route,
+  clear_id,
+  project_id,
+  target_currency,
+  level_bonus_check, 
+  ]);   
 
   //* 계정 테이블 업데이트
   currentQuery = `
@@ -109,80 +142,9 @@ export const updateUserLevelProcess = async (req, res) => {
     WHERE userkey = ?;`;
   updateQuery = mysql.format(currentQuery, [
     target_level,
-    target_experience,
+    target_experience < 0 ? 0 : target_experience,
     userkey,
   ]);
-
-  if (target_currency) {
-    // * 직접 입력
-    /*
-    currentQuery = `INSERT INTO user_mail(userkey, mail_type, currency, quantity, expire_date, connected_project) 
-      VALUES(?, 'levelup', ?, ?, DATE_ADD(NOW(), INTERVAL 1 YEAR), ?);`;
-    */
-
-    currentQuery = `CALL sp_insert_user_property(?,?,?,?);`;
-
-    sendQuery = mysql.format(currentQuery, [
-      userkey,
-      target_currency,
-      next_quantity,
-      "levelup",
-    ]);
-  }
-
-  //* 이벤트 기간(단발성) - 보너스 재화 
-  if(level_bonus_check > 0){
-    currentQuery = `INSERT INTO user_mail(userkey, mail_type, currency, quantity, expire_date, connected_project) 
-    VALUES(?, 'event', ?, ?, DATE_ADD(NOW(), INTERVAL 1 YEAR), -1);`;
-    if(target_level === 5){
-
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "gem",
-        "3",
-      ]);
-
-    }else if(target_level === 8){
-
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "gem",
-        "3",
-      ]);
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "coin",
-        "500",
-      ]);
-
-    }else if(target_level === 11){
-
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "gem",
-        "5",
-      ]);
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "coin",
-        "1000",
-      ]);      
-
-    }else if(target_level === 15){
-
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "gem",
-        "5",
-      ]);
-      sendQuery += mysql.format(currentQuery, [
-        userkey,
-        "coin",
-        "1500",
-      ]);
-
-    }
-  }
 
   result = await transactionDB(`
     ${insertQuery}
@@ -217,8 +179,8 @@ export const updateUserLevelProcess = async (req, res) => {
   if (target_currency) {
     //레벨업을 하는 경우, 보상 재화 전달
     responseData.reward = {
-      currency: next_currency,
-      quantity: next_quantity,
+      currency: target_currency,
+      quantity: target_quantity,
       icon_image_url,
       icon_image_key,
       currency_type,
