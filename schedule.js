ac8c9ee84416588c5e06e89b0cb501a5467a4d47
ije90 <ije90@pi-er.com>
2022-04-09 18:19:12 +0900

    등급 정산 작업중

--- schedule.js
+++ schedule.js
@@ -16,6 +16,9 @@ import {
 import {
   updateProjectViewCnt,
 } from "./controllers/coinController";
+import {
+  calculateGradeMonth, 
+} from "./controllers/gradeController";
 
 dotenv.config();
 
