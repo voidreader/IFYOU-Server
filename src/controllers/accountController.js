f1b5eb336ad4e3230ed4868051f9c6a27e5e85a9
ije90 <ije90@pi-er.com>
2022-04-19 16:05:27 +0900

    스페셜 에피소드 힌트 작업 완료

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2919,6 +2919,10 @@ const getProjectResources = async (project_id, lang, bubbleID, userkey) => {
     userkey,
     lang,
     userkey,
+    userkey, 
+    lang, 
+    userkey,
+    lang,
     project_id,
   ]); // 18. 에피소드 사이드 스토리
   query += mysql.format(Q_SELECT_EPISODE_PROGRESS, [userkey, project_id]); // 19. 에피소드 progress
@@ -3050,9 +3054,10 @@ const getProjectResources = async (project_id, lang, bubbleID, userkey) => {
   if (result.row[23].length > 0){
     // eslint-disable-next-line no-restricted-syntax
     for (const item of result.row[23]) {
-      const { ability_condition, } = item;
+      let { ability_condition, } = item;
       const abilitys = [];
 
+      ability_condition = ability_condition.toString().replace(" ", ""); //공백 모두 제거
       if(ability_condition){
         const abilityArr = ability_condition.split(",");  //값이 여러개인 경우 ,(콤마) 기준으로 split
         
@@ -3083,6 +3088,31 @@ const getProjectResources = async (project_id, lang, bubbleID, userkey) => {
       }
 
       item.ability_condition = abilitys;
+    }
+  }
+
+  //스페셜 에피소드 힌트 관련 에피소드 추가 
+  if (result.row[18].length > 0){
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of result.row[18]) {
+      const { side_hint, } = item; 
+      const hints = [];
+
+      if(side_hint !== ""){
+        const hintArr = String(side_hint).split(",");
+        for(let i = 0; i < hintArr.length; i++){
+          if(String(side_hint).includes(":")){        //사건
+            hints.push({
+              episode_id : hintArr[i].split(":")[0],  //에피소드ID
+              played: hintArr[i].split(":")[1],       //플레이건수
+              total: hintArr[i].split(":")[2],        //토탈
+            });
+          }else{                                      //에피소드
+            hints.push(hintArr[i]);
+          }
+        }
+      }
+      item.side_hint = hints;
     }
   }
 
