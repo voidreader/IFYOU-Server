0c88183ca181f1b25c0155562e653d789c6c82b6
ije90 <ije90@pi-er.com>
2022-04-27 21:32:31 +0900

    전체 미션 보상 받기

--- src/controllers/missionController.js
+++ src/controllers/missionController.js
@@ -109,4 +109,84 @@ export const userMisionReceive = async (req, res) => {
   res.status(200).json(responseData);
 };
 
+
+//! 전체 미션 보상 
+export const requestMissionAllReward = async(req, res) => {
+  
+  const {
+    body:{
+      userkey, 
+      project_id = -1, 
+      lang = "KO",
+    }
+  } = req;
+
+  const responseData = {};
+  let all_clear = 0;
+  let result = ``;
+  let currentQuery = ``; 
+  let updateQuery = ``;
+
+
+  //이미 전체 보상을 받은 경우 
+  result = await DB(`SELECT * FROM user_mission_all_clear WHERE userkey = ? AND project_id = ?;`, [userkey, project_id]);
+  if(result.state && result.row.length > 0){
+    logger.error(`requestMissionAllReward Error`);
+    respondDB(res, 80025, "already received reward.");
+    return;
+  }
+
+  //전체 미션 달성 확인 
+  result= await DB(`
+  SELECT 
+  fn_check_mission_all_clear(?, ?, ?) all_clear
+  FROM DUAL;
+  `, [userkey, project_id, lang]);
+  if(result.state && result.row.length > 0) all_clear = result.row[0].all_clear;
+
+  //전체 보상 경험치 
+  result = await DB(`SELECT ifnull(mission_exp, 0) mission_exp FROM list_project_master WHERE project_id = ?;`, [project_id]);
+  responseData.mission_exp = result.row[0].mission_exp;
+
+  //전체 보상 재화 
+  result = await DB(`
+  SELECT 
+  a.currency
+  , a.quantity
+  , b.icon_image_id
+  , fn_get_design_info(b.icon_image_id, 'url') icon_image_url 
+  , fn_get_design_info(b.icon_image_id, 'key') icon_image_key
+  FROM list_project_mission a, com_currency b
+  WHERE a.currency = b.currency
+  AND a.project_id = ? 
+  ORDER BY a.sortkey; 
+  `, [project_id]);
+  responseData.reward = result.row; 
+
+  responseData.bank = {};
+  console.log(`all_clear : ${all_clear}`);
+  if(all_clear === 1){
+    currentQuery = `CALL sp_insert_user_property(?, ?, ?, ?);`;
+    // eslint-disable-next-line no-restricted-syntax
+    for(const item of result.row){
+      updateQuery += mysql.format(currentQuery, [userkey, item.currency, item.quantity, 'mission_all']);
+    }
+
+    currentQuery = `INSERT INTO user_mission_all_clear(userkey, project_id) VALUES(?, ?);`;
+    updateQuery += mysql.format(currentQuery, [userkey, project_id]);
+
+    result = await transactionDB(updateQuery); 
+    if(!result.state){
+      logger.error(`requestMissionAllReward Error ${result.error}`);
+      respondDB(res, 80026, result.error);
+      return;      
+    }
+
+    responseData.bank = await getUserBankInfo(req.body);
+  }
+ 
+  res.status(200).json(responseData);
+
+};
+
 ////////// 클라이언트 호출 종료 /////////////////
