c598d41cca6b0fb117a6820a18c7b0e771e10745
Radiogaga.jin <radiogaga.jin@gmail.com>
2022-05-11 10:45:34 +0900

    pm2 클러스터, 스케줄링 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "live-pm2", // pm2 name
       script: "./dist/init.js", // // 앱 실행 스크립트
-      instances: "max", // 클러스터 모드 사용 시 생성할 인스턴스 수
+      instances: 2, // 클러스터 모드 사용 시 생성할 인스턴스 수
       exec_mode: "cluster", // fork, cluster 모드 중 선택
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
       autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
