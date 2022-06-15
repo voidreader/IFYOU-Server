9de1b70ea4cecc3634caf36c15d35341b3a29dbb
Radiogaga.jin <radiogaga.jin@gmail.com>
2022-06-15 14:19:18 +0900

    Update likeController.js

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -8,7 +8,7 @@ import { getUserBankInfo } from "./bankController";
 //! 좋아요 리스트
 export const getUserProjectLikeList = async (userkey) => {
   const like = [];
-  const result = await slaveDB(
+  const result = await DB(
     `
     SELECT project_id 
     FROM user_project_like 
@@ -19,12 +19,9 @@ export const getUserProjectLikeList = async (userkey) => {
   );
 
   // eslint-disable-next-line no-restricted-syntax
-  for (const item of result.row) {
-    //배열 처리
-    if (!like.includes(item.project_id)) {
-      like.push(item.project_id);
-    }
-  }
+  result.row.forEach((item) => {
+    like.push(item.project_id);
+  });
 
   return like;
 };
@@ -41,7 +38,7 @@ export const updateProjectLike = async (req, res) => {
     return;
   }
 
-  let isLike = "0";
+  let isLike = 0;
 
   //* 좋아요 등록/해제 처리
   let result = await DB(
@@ -54,14 +51,14 @@ export const updateProjectLike = async (req, res) => {
       `INSERT INTO user_project_like(userkey, project_id) VALUES(?, ?);`,
       [userkey, project_id]
     );
-    isLike = "1";
+    isLike = 1;
   } else {
     //있으면 delete
     result = await DB(
       `DELETE FROM user_project_like WHERE userkey = ? AND project_id = ?;`,
       [userkey, project_id]
     );
-    isLike = "0";
+    isLike = 0;
   }
 
   if (!result.state) {
@@ -70,18 +67,19 @@ export const updateProjectLike = async (req, res) => {
     return;
   }
 
+  const likeProject = await getUserProjectLikeList(userkey);
+
   const responseData = {};
-  responseData.like = await getUserProjectLikeList(userkey);
-  responseData.is_notify = "0";
+  responseData.like = likeProject;
+  responseData.is_notify = isLike;
 
   // 선호작 등록/해제에 따라서 작품 알림설정 처리
-  /*
+
   DB(`
     INSERT INTO user_project_notification (userkey, project_id, is_notify, last_modified)
     VALUES (${userkey}, ${project_id}, ${isLike}, now()) ON DUPLICATE KEY 
     UPDATE is_notify = ${isLike}, last_modified = now();
     `);
-  */
 
   res.status(200).json(responseData);
 };
